c638ffa6c9209346f1a4eff6fb4338cc9c44a891
Daniel Dytert <daniel.dytert@hanseaticbank.de>
2024-03-09 08:37:54 +0100

    add OneMovie and OneMovieForEdit db functions

--- cmd/api/internal/repository/repository.go
+++ cmd/api/internal/repository/repository.go
@@ -8,6 +8,9 @@ import (
 type DatabaseRepo interface {
 	Connection() *sql.DB
 	AllMovies() ([]*models.Movie, error)
+	OneMovie(id int) (*models.Movie, error)
+	OneMovieForEdit(id int) (*models.Movie, []*models.Genre, error)
+
 	GetUserByEmail(string) (*models.User, error)
 	GetUserByID(id int) (*models.User, error)
 }
