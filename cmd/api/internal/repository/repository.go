7528d8e72e990cced2d4d6ca413b3681c887633f
Daniel Dytert <daniel.dytert@hanseaticbank.de>
2024-04-20 09:27:57 +0200

    fetch movies by genre

--- cmd/api/internal/repository/repository.go
+++ cmd/api/internal/repository/repository.go
@@ -8,7 +8,7 @@ import (
 type DatabaseRepo interface {
 	Connection() *sql.DB
 
-	AllMovies() ([]*models.Movie, error)
+	AllMovies(genre ...int) ([]*models.Movie, error)
 	OneMovie(id int) (*models.Movie, error)
 	OneMovieForEdit(id int) (*models.Movie, []*models.Genre, error)
 	InsertMovie(movie models.Movie) (int, error)
